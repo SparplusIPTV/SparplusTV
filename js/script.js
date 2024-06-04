43dd58dbdac10c79fc0ede5dc94e380ca0e5d297
SparplusIPTV <info@iptvstarplus.net>
2024-06-04 20:08:37 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let bar = document.getElementById("bar")
+let navbars = document.getElementById("nv")
+bar.onclick = function(){
+    navbars.classList.toggle("active")
+}
+
+var acc = document.getElementsByClassName
+("accordion");
+var i;
+
+for(i = 0; i < acc.length ; i++){
+    acc[i].addEventListener("click", function 
+    (){
+        this.classList.toggle("active");
+        this.parentElement.classList.toggle
+        ("active");
+
+        var pannel= this.nextElementSibling;
+
+        if(pannel.style.display === "block"){
+            pannel.style.display = "none";
+        }else{
+            pannel.style.display = "block";
+
+        }
+    })
+}
